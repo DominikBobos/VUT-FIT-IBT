c448b6ca73f71c6221051a99593cc40ce5cee4f1
Dominik Boboš <48943382+DominikBobos@users.noreply.github.com>
2020-09-29 00:08:28 +0200

    ver1

---
+++ scripts/mix.py
@@ -0,0 +1,191 @@
+##
+#   @file mix.py
+#   @author Dominik Bobos 
+#   @brief Script for mixing pre-recorded messages with speech examples
+#
+
+from pydub import AudioSegment          # for splitting, mixing audio files
+from pydub import effects               # for changing speed
+from librosa.core import load           # for loading audio files
+import math                             # for rounding numbers
+import argparse, textwrap               # for parsing arguments
+import os, sys, glob, re                # for file path, finding files, etc
+import random as rand 	                # for random numbers generator
+
+
+"""''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Arguments parsing
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"""
+parser = argparse.ArgumentParser( formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("-m", "--mode", help=textwrap.dedent("""Specifies which type of pre-recorded message you want to use.
+  Possible values are: A, B, C"""))
+parser.add_argument("-g", "--gain", action="store_true", help="Change volume gain of the pre-recorded message")                            
+parser.add_argument("-s", "--speed", action="store_true", help="Change speed of the pre-recorded message")                       
+parser.add_argument("--stats", action="store_true", 
+	help="show statistics about the files in either source or destination directory")
+parser.add_argument("-r", "--repeat", action="store_true", help="Cut or repeat the pre-recorded messages")    
+parser.add_argument("--mpath", help="Path to the directory with the pre-recorded messages")  
+parser.add_argument("--spath", help="Path to the directory with the speech .wav files")   
+parser.add_argument("--export", help="Path to the directory where to export the mixed audio files")
+parser.add_argument("--lt", help="Will use longer recordings than specified in seconds")    # use audio longer than specified in seconds
+parser.add_argument("--st", help="Will use shorter recordings than specified in seconds")   # use audio shorter than specified in seconds
+parser.add_argument("--random", default=0, help="""Select severity of randomness
+Possible values are:
+  0 = no random values, use the original values of the pre-recorded message
+  1 = low differences, e.g. gain between -3dB+3dB, speed 0.95-1.05, repeat 1-10.0
+  2 = optimal differences, e.g. gain between -6dB+6dB, speed 0.85-1.15, repeat 0.9-30.0
+  3 = high differences, e.g. gain between -10dB+10dB, speed 0.8-1.2, repeat 0.8-50.0""") 
+arguments = parser.parse_args()
+
+if arguments.mode and not re.match(r"^(a|b|c|A|B|C)([0-9][0-9])$|^(a|b|c|A|B|C)$", arguments.mode): 
+	sys.stderr.write("Invalid mode! Use help for more information\n")
+	sys.exit(1)
+elif arguments.mode != None:
+	mode = arguments.mode.upper() 
+	print(mode)
+else:
+	mode = ''
+
+if arguments.random:
+	try:
+		if int(arguments.random) != 0 and \
+		int(arguments.random) != 1 and \
+		int(arguments.random) != 2 and \
+		int(arguments.random) != 3:
+			sys.stderr.write("Invalid random value! Use help for more information\n")
+			sys.exit(1)
+		else:
+			random = int(arguments.random)
+	except:
+		sys.stderr.write("Invalid random value! Use help for more information\n")
+		sys.exit(1)
+
+if not arguments.mpath:
+	msgsrc = os.getcwd()
+else:
+	if os.path.isdir(arguments.mpath):
+		msgsrc = arguments.mpath
+	else:
+		sys.stderr.write("Invalid mpath directory.\n")
+		sys.exit(1)
+
+if not arguments.spath:
+	recsrc = os.getcwd()
+else:
+	if os.path.isdir(arguments.spath):
+		recsrc = arguments.spath
+	else:
+		sys.stderr.write("Invalid spath directory.\n")
+		sys.exit(1)
+
+if not arguments.export:
+	expsrc = os.getcwd()
+else:
+	if os.path.isdir(arguments.export):
+		expsrc = arguments.export
+	else:
+		try:
+			os.mkdir(arguments.export)                                  # create destination directory
+			expsrc = arguments.export
+		except:
+			sys.stderr.write("Invalid directory for export.\n")
+			sys.exit(1)
+
+if random == 0:
+	GAIN_RAND = [0.0, 0.0]
+	SPEED_RAND = [1.0, 1.0]
+	REPEAT_RAND = [1.0, 1.0]
+elif random == 1:
+	GAIN_RAND = [-3.0, +3.0]
+	SPEED_RAND = [0.95, 1.05]
+	REPEAT_RAND = [1.0, 10.0]
+elif random == 2:
+	GAIN_RAND = [-6.0, +6.0]
+	SPEED_RAND = [0.85, 1.15]
+	REPEAT_RAND = [0.9, 30.0]
+elif random == 3:
+	GAIN_RAND = [-10.0, +10.0]
+	SPEED_RAND = [0.8, 1.2]
+	REPEAT_RAND = [0.8, 50.0]
+
+
+
+"""''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Gets duration in seconds from the given audio file
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"""
+def get_duration(file):
+	return AudioSegment.from_wav(file).duration_seconds
+
+
+def modify(file):
+	mixedFile = AudioSegment.from_wav(file)
+	gRand, rRand, sRand = 0,1,1
+	if arguments.gain:
+		gRand = rand.uniform(GAIN_RAND[0], GAIN_RAND[1])
+		mixedFile += gRand
+	if arguments.repeat:
+		rRand = rand.uniform(REPEAT_RAND[0], REPEAT_RAND[1])
+		floorInt = math.floor(rRand)
+		left = rRand - floorInt
+		if floorInt != 0: 
+			mixedFile = mixedFile * floorInt + mixedFile[:math.ceil(left*get_duration(file)*1000)]
+		else:
+			mixedFile = mixedFile[:math.ceil(left*get_duration(file)*1000)]
+	if arguments.speed:
+		sRand = rand.uniform(SPEED_RAND[0], SPEED_RAND[1])
+		mixedFile = effects.speedup(seg=mixedFile, playback_speed=sRand)
+	return file, mixedFile, mixedFile.duration_seconds ,gRand, rRand, sRand
+		# mixedFile.export(arguments.export + file.split('/')[-1], format="wav")
+
+
+def get_type(file):
+	try:
+		fileType = file.split('/')
+		fileType = fileType[-1][0] #first letter shows type of the pre-recorded message
+	except:
+		fileType = file[0]
+	return fileType
+
+
+def make_filename(recordFile, )
+
+
+def mix():
+	msg = glob.glob(msgsrc + mode + '*.wav')
+	rec = glob.glob(recsrc + '*.wav')	
+	modifiedList = []
+	for file in msg:
+		modifiedList.append(modify(file))
+	index = 0
+	for file in rec:
+		#filter out unwanted files
+		if arguments.lt and int(arguments.lt) > get_duration(file):
+			continue
+		if arguments.st and int(arguments.st) < get_duration(file):
+			continue
+		record = AudioSegment.from_wav(file)
+		if get_type(modifiedList[index][0]) == 'A':		#add to the beginning
+			record = modifiedList[index][1] + record 
+			position = 0
+		elif get_type(modifiedList[index][0]) == 'B':		#add somewhere in between
+			randCut = math.ceil(rand.uniform(0.0, get_duration(file)))
+			record = record[:randCut*1000] + modifiedList[index][1] + record[randCut*1000:]
+			position = randCut
+		else:	#type 'C'	            #add at the end
+			record = record + modifiedList[index][1] 
+			position = get_duration(file)
+
+		# print(file, modifiedList[index])
+		filename = file.split('/')[-1].split('.w')[0] + '__' + modifiedList[index][0].split('/')[-1].split('.w')[0] + \
+		'_ST(' + "{:.2f}".format(position) + ')L(' + "{:.2f}".format(modifiedList[index][2]) + ')G(' + \
+		"{:.2f}".format(modifiedList[index][3]) + ')R(' + "{:.2f}".format(modifiedList[index][4]) + \
+		')S(' + "{:.2f}".format(modifiedList[index][5]) + ').wav'
+		record.export(arguments.export + filename, format="wav")
+		# name it the right way and export
+		index += 1
+		if index == len(modifiedList):
+			index = 0
+
+
+
+mix()
