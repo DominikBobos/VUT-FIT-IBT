83cfbb80afdfd92751618b36bf2e135c573c2bfb
Dominik Bobos <bobos.dominik@gmail.com>
2020-11-09 00:29:20 +0100

    HTK script added

---
+++ scripts/loadHTK.py
@@ -0,0 +1,9 @@
+from HTK import HTKFile
+import numpy as np
+import sys
+
+htk_reader = HTKFile()
+ret = htk_reader.load(sys.argv[1])
+print(htk_reader.nSamples, htk_reader.nFeatures, htk_reader.sampPeriod, htk_reader.qualifiers)
+result = np.array(htk_reader.data)
+print(result.shape, result)
---
+++ scripts/move.sh
@@ -0,0 +1,8 @@
+python3 split.py --src=/home/dominik/Desktop/bak/cut/ --dst=/home/dominik/Desktop/bak/moved/ --count=150 --move --lt=121 --st=200
+python3 split.py --src=/home/dominik/Desktop/bak/cut/ --dst=/home/dominik/Desktop/bak/moved/ --count=250 --move --lt=91 --st=121
+python3 split.py --src=/home/dominik/Desktop/bak/cut/ --dst=/home/dominik/Desktop/bak/moved/ --count=350 --move --lt=61 --st=91
+python3 split.py --src=/home/dominik/Desktop/bak/cut/ --dst=/home/dominik/Desktop/bak/moved/ --count=350 --move --lt=46 --st=61
+python3 split.py --src=/home/dominik/Desktop/bak/cut/ --dst=/home/dominik/Desktop/bak/moved/ --count=460 --move --lt=31 --st=46
+python3 split.py --src=/home/dominik/Desktop/bak/cut/ --dst=/home/dominik/Desktop/bak/moved/ --count=600 --move --lt=21 --st=31
+python3 split.py --src=/home/dominik/Desktop/bak/cut/ --dst=/home/dominik/Desktop/bak/moved/ --count=1100 --move --lt=16 --st=21
+python3 split.py --src=/home/dominik/Desktop/bak/cut/ --dst=/home/dominik/Desktop/bak/moved/ --count=1000 --move --lt=14 --st=16
