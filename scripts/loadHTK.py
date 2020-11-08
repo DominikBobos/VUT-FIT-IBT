88bdb6bc830b55ba70e5045c15780f13de6a341c
Dominik Bobos <bobos.dominik@gmail.com>
2020-11-09 00:30:28 +0100

    added credits

--- scripts/loadHTK.py
+++ scripts/loadHTK.py
@@ -2,6 +2,9 @@ from HTK import HTKFile
 import numpy as np
 import sys
 
+
+#credits: https://github.com/danijel3/PyHTK
+
 htk_reader = HTKFile()
 ret = htk_reader.load(sys.argv[1])
 print(htk_reader.nSamples, htk_reader.nFeatures, htk_reader.sampPeriod, htk_reader.qualifiers)
